09efb3430153ddae7bf439b111cb7e0dc4499771
gowtham <gowtham.gthm@gmail.com>
2023-12-22 01:46:04 +0530

    changes for the formatting

--- javascript/js-ops.js
+++ javascript/js-ops.js
@@ -6,5 +6,18 @@ $(document).ready(function () {
             elements[i].classList.toggle("remove-item");
         }
 
+        var leftDiv = document.getElementsByClassName("left-content");
+
+        for( i=0 ; i< leftDiv.length ; i++) {
+            leftDiv[i].classList.toggle("left-content-decrease");
+        }
+
+        var subHeader = document.getElementById("sub-header");
+        subHeader.classList.toggle("remove-item");
+
+        var header = document.getElementById("divider");
+        header.classList.toggle("margin-below");
+
+
     });
 });
